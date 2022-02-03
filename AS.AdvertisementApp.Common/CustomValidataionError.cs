ee0b019da6cc4687618043de3d3be27955c9e4f6
alihan.sari <alihan.sari@dr.com.tr>
2022-02-03 10:37:40 +0300

    Add to Project Entities Layer

---
+++ AS.AdvertisementApp.Common/CustomValidataionError.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS.AdvertisementApp.Common
+{
+    public class CustomValidataionError
+    {
+        public int PropertyName { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
---
+++ AS.AdvertisementApp.Entities/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace AS.AdvertisementApp.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
