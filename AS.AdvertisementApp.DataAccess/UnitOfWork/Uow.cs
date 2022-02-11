ef095dffa4831010f43577c6d6d50ad7955d9930
alihan.sari <alihan.sari@dr.com.tr>
2022-02-11 14:31:57 +0300

    Repository and UnitofWork Added

---
+++ AS.AdvertisementApp.DataAccess/UnitOfWork/Uow.cs
@@ -0,0 +1,29 @@
+﻿using AS.AdvertisementApp.DataAccess.Contexts;
+using AS.AdvertisementApp.DataAccess.Interfaces;
+using AS.AdvertisementApp.DataAccess.Repositories;
+using AS.AdvertisementApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS.AdvertisementApp.DataAccess.UnitOfWork
+{
+    public class Uow
+    {
+        private readonly AdvertisementContext _context;
+        public Uow(AdvertisementContext context)
+        {
+            _context = context;
+        }
+        public IRepository<T> GetRepository<T>() where T : BaseEntity
+        {
+            return new Repository<T>(_context);
+        }
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
